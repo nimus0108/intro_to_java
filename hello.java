2a89abae516b94168bc18531ee9800ec453d296d
nimus0108 <nimus0108@gmail.com>
2014-12-11 21:38:06 -0500

    added hello.java (12/11)

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class hello {
+	boolean isAClass = true; 
+	static int someWholdNumber = 18;
+	
+public static void main(String[] args) {
+	System.out.println("Hello world!");
+	System.out.println("Wassup");
+	System.out.println("hey guys what am i supposed to write here");
+}
+}
